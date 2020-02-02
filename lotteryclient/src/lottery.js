2c4b82d06c313eca9f29025a7ddd71ad47d2501a
ashsometimes <aishsahoo5@gmail.com>
2020-02-02 22:43:17 +0530

    contract + frontend completed

---
+++ lotteryclient/src/lottery.js
@@ -0,0 +1,6 @@
+import web3js from './web3'
+
+const address = '0x0517b49E24211AD22BBAf8A30bf4D21EC1a7501a'
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}] 
+
+export default new web3js.eth.Contract(abi, address)
